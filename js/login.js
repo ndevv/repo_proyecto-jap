e77a9e0d335a4b36a5784789c46ea61ab5f15c89
DiegoG77 <guarino_diego@yahoo.com>
2020-08-16 23:42:27 -0300

    Entrega 1
    Nota: Cambié el nombre del archivo "login" por "index".
    Y el nombre del archivo "index" por "index2".

--- js/login.js
+++ js/login.js
@@ -2,5 +2,25 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    
+    document.getElementById("submitBtn").addEventListener("click", function (e) {
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+
+        if (inputEmail.value === '') {
+            inputEmail.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (inputPassword.value === '') {
+            inputPassword.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (camposCompletos) {
+            window.location = 'index2.html';
+        }
+    });
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var productsArray = [];
+
+function showProductsList(array) {
+
+    let contenido = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        contenido += `
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.cost + ` USD</small>
+                    </div>
+                    <p class="mb-1">` + product.description + `</p>
+                </div>
+            </div>
+        
+        `
+
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsList(productsArray);
+        }
+    });
 });
