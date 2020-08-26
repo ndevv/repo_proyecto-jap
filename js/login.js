09d88fca620ef96613d0e529b75b62cff0254f78
DiegoG77 <guarino_diego@yahoo.com>
2020-08-26 00:22:53 -0300

    login
    Autenticación Google

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    
+document.addEventListener("DOMContentLoaded", function (e) {
+
     document.getElementById("submitBtn").addEventListener("click", function (e) {
         let inputEmail = document.getElementById("inputEmail");
         let inputPassword = document.getElementById("inputPassword");
@@ -24,3 +24,16 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 
 });
+
+function onSignIn(googleUser) {
+
+    var profile = googleUser.getBasicProfile();
+    //var id_token = googleUser.getAuthResponse().id_token;
+    localStorage.setItem('Name',profile.getName());
+
+    if(localStorage.getItem('Name') != undefined || sessionStorage.getItem('Name') != null){
+        localStorage.setItem('Name',profile.getGivenName());
+        localStorage.setItem('Email',profile.getEmail());
+        window.location.href="index2.html";
+    }  
+}
